3a22bcb7b90354de7a546a756ee836c0e96da506
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-13 00:34:02 +0200

    module 4 final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        switch (year % 4) {
+            case 0:
+
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int quotient = dividend % divider;
+
+        if (divider != 0) {
+            if (quotient != 0) {
+                int product = quotient * divider;
+                if (product == dividend) {
+                    System.out.println("can be divided completely");
+                } else if (quotient != dividend) {
+                    System.out.println("cannot be divided completely");
+                }
+                }
+            }else {
+            System.out.println("division by zero");
+        }
     }
 }
